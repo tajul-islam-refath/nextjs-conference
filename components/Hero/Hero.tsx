e0ebc295ba1590f4eda54beb3d832d4ef573c3b7
Tajul Islam Refath <tajul16-447@diu.edu.bd>
2023-09-24 22:28:20 +0600

    redesign hero section

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -8,34 +8,32 @@ import hero6 from "../../public/hero_6.jpg";
 
 function Hero() {
   return (
-    <div className="hero min-h-screen">
+    <div className="hero">
       <Image src={hero1} className="hero__img--one" alt="" />
       <Image src={hero2} className="hero__img--two" alt="" />
       <Image src={hero5} className="hero__img--five" alt="" />
       <Image src={hero6} className="hero__img--six" alt="" />
       <div className="container mx-auto">
         <div className="hero__content">
-          <div className="row">
-            <div className="col-md-7">
-              <div className="hero__text">
-                <h1>React</h1>
-                <h1>Conference</h1>
-              </div>
-              <div className="row">
-                <div className="col-md-5">
-                  <Image src={hero4} alt="" className="hero4" />
-                </div>
-                <div className="col-md-7">
-                  <p>
-                    Lorem uis diam turpis quam id fermentum.In quis diam turpis
-                    quam id fermentum..id fermentum.In quis diam turpis quam id
-                    fermentum.
-                  </p>
-                  <button className="hero__buyticket">Buy Tickets</button>
-                </div>
+          <div className="hero__text">
+            <h1>React</h1>
+            <h1>Conference</h1>
+          </div>
+          <div className="flex flex-col md:flex-row justify-between">
+            <div className="w-full text-black md:w-1/3 order-3 md:order-1">
+              <Image src={hero4} alt="" className="hero4" />
+            </div>
+            <div className="w-full text-black md:w-1/3  order-1 md:order-2">
+              <div className="">
+                <p>
+                  Lorem uis diam turpis quam id fermentum.In quis diam turpis
+                  quam id fermentum..id fermentum.In quis diam turpis quam id
+                  fermentum.
+                </p>
+                <button className="hero__buyticket">Buy Tickets</button>
               </div>
             </div>
-            <div className="col-md-5">
+            <div className="w-full text-black md:w-1/3 order-2 md:order-3">
               <Image src={hero3} alt="" className=" hero3" />
             </div>
           </div>
