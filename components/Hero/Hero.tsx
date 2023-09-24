a61267689f0e0c93c810410dd6e1eaf3f9f6cb66
Tajul Islam Refath <tajul16-447@diu.edu.bd>
2023-09-24 22:57:43 +0600

    make project responsive

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import hero1 from "../../public/hero_1.png";
 import hero2 from "../../public/hero_2.png";
+import hero2M from "../../public/hero_2_m.png";
 import hero3 from "../../public/hero_3.png";
 import hero4 from "../../public/hero_4.png";
 import hero5 from "../../public/hero_5.png";
@@ -11,6 +12,7 @@ function Hero() {
     <div className="hero">
       <Image src={hero1} className="hero__img--one" alt="" />
       <Image src={hero2} className="hero__img--two" alt="" />
+      <Image src={hero2M} className="hero__img--two-m" alt="" />
       <Image src={hero5} className="hero__img--five" alt="" />
       <Image src={hero6} className="hero__img--six" alt="" />
       <div className="container mx-auto">
@@ -20,10 +22,10 @@ function Hero() {
             <h1>Conference</h1>
           </div>
           <div className="flex flex-col md:flex-row justify-between">
-            <div className="w-full text-black md:w-1/3 order-3 md:order-1">
+            <div className="w-full  md:w-1/3 order-3 md:order-1">
               <Image src={hero4} alt="" className="hero4" />
             </div>
-            <div className="w-full text-black md:w-1/3  order-1 md:order-2">
+            <div className="w-full my-5 md:my-0  md:w-1/3  order-1 md:order-2">
               <div className="">
                 <p>
                   Lorem uis diam turpis quam id fermentum.In quis diam turpis
@@ -33,7 +35,7 @@ function Hero() {
                 <button className="hero__buyticket">Buy Tickets</button>
               </div>
             </div>
-            <div className="w-full text-black md:w-1/3 order-2 md:order-3">
+            <div className="w-full md:w-1/3 order-2 md:order-3">
               <Image src={hero3} alt="" className=" hero3" />
             </div>
           </div>
